960f4bbf37cb10c7cafd78356dc18c5480edf63b
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-07-30 14:17:36 +0530

    update

--- App/pages/medicine/Medicine.js
+++ App/pages/medicine/Medicine.js
@@ -1,24 +1,25 @@
-
-import React, { useEffect, useState, useContext } from 'react';
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
-import axios from 'axios'; 
-import image2 from '../../assets/images/image2.png';
-import image3 from '../../assets/images/image3.png';
-import image4 from '../../assets/images/image4.png';
-import image5 from '../../assets/images/image5.png';
-import Calendar from '../../components/calender/Calender';
-import { AuthContext } from '../../utilities/auth/AuthContext';
+import React, { useEffect, useState, useContext } from "react";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import axios from "axios";
+import image2 from "../../assets/images/image2.png";
+import image3 from "../../assets/images/image3.png";
+import image4 from "../../assets/images/image4.png";
+import image5 from "../../assets/images/image5.png";
+import Calendar from "../../components/calender/Calender";
+import { AuthContext } from "../../utilities/auth/AuthContext";
 
 export default function Medicine() {
   const [allMedicineData, setAllMedicineData] = useState([]);
   const { userInfo } = useContext(AuthContext);
-  
+
   const getAllMedicine = async () => {
     try {
-      const response = await axios.get('http://192.168.8.100:5001/api/medicine');
+      const response = await axios.get(
+        "http://192.168.8.100:5001/api/medicine"
+      );
       setAllMedicineData(response.data);
-    }catch (error) {
-      console.error('Error fetching medicine data:', error);
+    } catch (error) {
+      console.error("Error fetching medicine data:", error);
     }
   };
 
@@ -31,7 +32,7 @@ export default function Medicine() {
       <View key={index} style={styles.doseContainer}>
         <Text style={styles.doseTime}>{dose.time}</Text>
         <Text style={styles.doseMealTiming}>
-          {dose.mealTiming === 0 ? 'Before meal' : 'After meal'}
+          {dose.mealTiming === 0 ? "Before meal" : "After meal"}
         </Text>
       </View>
     ));
@@ -54,14 +55,20 @@ export default function Medicine() {
       </View>
       <FlatList
         data={allMedicineData}
-        keyExtractor={item => item._id}
+        keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
           <View style={styles.reminderBox}>
             <View style={styles.imageCircle}>
-              <Image style={styles.image5} resizeMode="contain" source={image5} />
+              <Image
+                style={styles.image5}
+                resizeMode="contain"
+                source={image5}
+              />
             </View>
             <View style={styles.reminderText}>
-              <Text style={styles.pillText}>{item.amount} {item.selectedMedicine}</Text>
+              <Text style={styles.pillText}>
+                {item.amount} {item.selectedMedicine}
+              </Text>
               <Text style={styles.medicineText}>{item.medicineName}</Text>
               {renderDoseDetails(item.doses)}
             </View>
@@ -77,17 +84,17 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 30,
     paddingHorizontal: 20,
-    backgroundColor: "#FFFFFF"
+    backgroundColor: "#FFFFFF",
   },
   topText: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 7
+    marginBottom: 7,
   },
   helloText: {
     fontSize: 28,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   image2: {
     height: 41,
@@ -101,17 +108,17 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginTop: 23
+    marginTop: 23,
   },
   takeText: {
     fontSize: 26,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   middleBorder: {
     backgroundColor: "#D9D9D9",
     width: 63,
     height: 31,
-    borderRadius: 70,
+    borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
@@ -122,10 +129,10 @@ const styles = StyleSheet.create({
   },
   reminderBox: {
     backgroundColor: "#D9D9D9",
-    width: '100%',
-    borderRadius: 15,
-    flexDirection: 'row',
-    alignItems: 'center',
+    width: "100%",
+    borderRadius: 60,
+    flexDirection: "row",
+    alignItems: "center",
     padding: 10,
     marginBottom: 10,
   },
@@ -135,24 +142,26 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     backgroundColor: "#FFFFFF",
     marginRight: 35,
-    alignItems: "center"
+    alignItems: "center",
   },
   image5: {
     height: 50,
-    width: 50,
+    width: 80,
     marginTop: 15,
   },
   reminderText: {
     flex: 1,
+    
   },
   pillText: {
     fontSize: 17,
-    fontWeight: '700',
+    fontWeight: "700",
+    
   },
   medicineText: {
     fontSize: 18,
-    color: '#000000',
-    fontWeight: '600',
+    color: "#000000",
+    fontWeight: "600",
   },
   doseContainer: {
     marginTop: 4,
@@ -160,16 +169,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-center",
   },
   doseTime: {
-    fontSize: 15,
-    color: '#000000',
+    fontSize: 16,
+    color: "#000000",
     marginRight: 10,
   },
   doseMealTiming: {
     fontSize: 13,
-    color: '#555',
+    color: "#555",
   },
- 
 });
-
-
-
