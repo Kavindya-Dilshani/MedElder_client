08891f88f025794c248f7a8ac84dad3e5be09230
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-08-01 23:37:08 +0530

    create emergencyEvents page

---
+++ App/pages/emergencyEvent/EmergencyEvents.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EmergencyEvents = () => {
+  return (
+    <View>
+      <Text>EmergencyEvents</Text>
+    </View>
+  )
+}
+
+export default EmergencyEvents
+
+const styles = StyleSheet.create({})
