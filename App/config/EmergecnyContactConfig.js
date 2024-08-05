d52bec8e2b147ba680518d232c16d034ecfed280
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-08-05 21:16:00 +0530

    create emergencyContactConfig

---
+++ App/config/EmergecnyContactConfig.js
@@ -0,0 +1,12 @@
+import { phone } from "react-native-vector-icons/FontAwesome"; 
+
+const emergencyContacts = [
+    { title: "Ambulance", number: "110", icon: phone },
+    { title: "Police Emergency Hotline", number: "118/119", icon: phone },
+    { title: "Accident Service-General Hospital-Colombo", number: "011-2691111", icon: phone },
+    { title: "National Help Desk", number: "118", icon: phone },
+    { title: "Government Help", number: "1919", icon: phone },
+    { title: "Red Cross", number: "011-2672727", icon: phone },
+];
+
+export default EmergencyContactConfig = { emergencyContacts };
