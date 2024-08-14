d310facd26462b47b26f8b3364176fb40ff026e8
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-08-15 00:12:47 +0530

    add destination api

--- App/utilities/GlobalApi.js
+++ App/utilities/GlobalApi.js
@@ -1,11 +1,17 @@
+
 import axios from "axios";
 
-const BASE_URL = "https://maps.googleapis.com/maps/api/place";
+const BASE_URL = "https://maps.googleapis.com/maps/api";
 const API_KEY = "AIzaSyCYmVWirR1Tz3ENUdW1OmaZRd0nJ1acVeI";
 
 const nearByPlace = (lat, lng) =>
   axios.get(
-    `${BASE_URL}/nearbysearch/json?location=${lat},${lng}&radius=1500&type=hospital&key=${API_KEY}`
+    `${BASE_URL}/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=hospital&key=${API_KEY}`
   );
 
-export default { nearByPlace };
+const getDistanceAndDuration = (originLat, originLng, destinationLat, destinationLng) =>
+  axios.get(
+    `${BASE_URL}/distancematrix/json?origins=${originLat},${originLng}&destinations=${destinationLat},${destinationLng}&key=${API_KEY}`
+  );
+
+export default { nearByPlace, getDistanceAndDuration };
